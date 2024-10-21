cfc888ba45ec377284f4737b3364274ae2e3b657
Salem H <salemkassal@gmail.com>
2024-10-21 14:32:53 -0400

    exercise2

--- object_scripts/products_code.js
+++ object_scripts/products_code.js
@@ -1,1 +1,22 @@
-function parsePartCode()
+const product_string = "XYZ:1234-l";
+
+function parsePartCode(part){
+let pieces_array = part.split(":");
+
+let final_two_string = pieces_array[1];
+let final_two_array = final_two_string.split("-");
+
+return {
+    supplierCode : pieces_array[0],
+    productNumber : final_two_array[0],
+    sixe : final_two_string[1]
+  }
+}
+
+let partObject = parsePartCode(product_string)
+
+console.log(`
+    CODE: ${partObject.supplierCode}
+    NUMBER: ${partObject.productNumber}
+    SIZE: ${partObject.size}
+    `)
